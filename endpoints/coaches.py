4bb13bc8e383a28917a2d0daa7b0fa187778c942
DMcDonald57 <d.mcdonald579@gmail.com>
2023-03-09 17:06:18 -0700

    backend log in procedure works

--- endpoints/coaches.py
+++ endpoints/coaches.py
@@ -6,7 +6,7 @@ import uuid
 
 #endpoints for coaches go here
 
-@app.post('/api/coaches')
+@app.post('/api/coachsession')
 def coach_login():
     email = request.json.get('email')
     password = request.json.get('password')
@@ -17,11 +17,7 @@ def coach_login():
     if storedpassword != password:
         return make_response(jsonify("Password does not match"),401)
     result = run_statement('CALL coach_login (?,?)', [coach_id, token])
-    if (type(result) == list):
-        response = {
-            "coachId" : result[0][0],
-            "token" : result[0][1]
-        }
-        return make_response(jsonify(response), 201)
+    if result == None:
+        return make_response(jsonify("You are logged in Coach"), 201)
     else:
-        return make_response(jsonify(result), 500)
+        return make_response(jsonify("Try again Coach"), 500)
